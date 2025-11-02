3dde5fa9e38cf3e91bed2c2c0e5224c1b46c7dca
Jörgen <jorgen.karlsson.91@gmail.com>
2025-11-02 14:46:33 +0100

    lade om pricepolicy på items

--- src/com/karlsson/entity/item/Piano.java
+++ src/com/karlsson/entity/item/Piano.java
@@ -1,10 +1,12 @@
 package com.karlsson.entity.item;
 
-public class Piano extends Instrument{
+import com.karlsson.entity.member.Member;
+
+public class Piano extends Item {
     private boolean grand;
 
-    public Piano(double pricePerDay, int stock, String brand, String model, boolean grand) {
-        super(pricePerDay, stock,  brand, model);
+    public Piano( int stock, String brand, String model, boolean grand) {
+        super(stock,  brand, model);
         this.grand = grand;
     }
     public boolean isGrand() {return grand;}
@@ -16,7 +18,7 @@ public class Piano extends Instrument{
     @Override
     public String getDisplayName() {
         String s = "Piano, " + getBrand() + " " + getModel();
-        if (grand) {
+        if (isGrand()) {
             s += ", flygel";
         }
         return s;
@@ -25,7 +27,22 @@ public class Piano extends Instrument{
     @Override
     public String toString() {
         return "Piano{" +
-                ", grand=" + grand +
+                ", grand=" + isGrand() +
                 '}';
     }
+
+    @Override
+    public double calculatePrice(Item item, int days, Member member) {
+        double price = 1000 * days;
+        double discount = 0.0;
+
+        switch (member.getLevel()) {
+            case STUDENT -> discount = 0.2; //student 20% rabatt
+            case PREMIUM -> discount = 0.5; //premium 50%
+            case STANDARD -> discount = 0.0; //standard ingen rabatt
+        }
+
+        return price * (1 - discount);
+    }
+
 }
